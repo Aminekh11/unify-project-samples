484e241cf65b051e38ca789cbdc56edae41a3dbd
avigor6 <93041802+Aminekh11@users.noreply.github.com>
2024-01-21 21:18:04 +0100

    unify

---
+++ Controller.user/checkSi.php
@@ -0,0 +1,46 @@
+<?php
+include('D:/Esprit 2eme/progs/xampp/htdocs/ss/config.php');
+session_start();
+
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+$Password =md5($Password);
+
+
+$con = config::getConnexion();
+$requete = "SELECT Id_User FROM user WHERE Username = :Username AND Pwd = :Pwd";
+$stmt = $con->prepare($requete);
+$stmt->bindParam(':Username', $Username);
+$stmt->bindParam(':Pwd', $Password);
+
+try {
+    $stmt->execute();
+} catch (PDOException $e) {
+    die('Erreur: ' . $e->getMessage());
+}
+
+$row = $stmt->fetchAll();
+
+if (!empty($row)) {
+    $_SESSION['valide'] = true;
+    $_SESSION['Username'] = $Username;
+    
+    
+
+    if ($Username == "admin" && $Password == "21232f297a57a5a743894a0e4a801fc3") {
+        header('Location: /ss/View/BackOffice/template/index.php');
+    } else {
+        $userId = $row[0]['Id_User'];
+        session_start();
+        $_SESSION['user_data'] = $row[0];
+        $_SESSION['id'] = $userId;
+        header('Location: /ss/View/FrontOfiice/View/Home/postDD.php');
+        
+        
+    }
+    exit(); // Ensure that no further code is executed after the redirect
+} else {
+    echo '<script>alert("Username or Password is invalid");</script>';
+    echo '<script>window.location.href="/ss/View/index.php";</script>';
+    exit(); // Ensure that no further code is executed after the redirect
+}
